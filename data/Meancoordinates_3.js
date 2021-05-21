9d73c8b7e680f084d4f5b2fcfc59f637ae1954e3
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-05-22 02:41:52 +0700

    Add files via upload

---
+++ data/Meancoordinates_3.js
@@ -0,0 +1,8 @@
+var json_Meancoordinates_3 = {
+"type": "FeatureCollection",
+"name": "Meancoordinates_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "MEAN_X": 108.49251885, "MEAN_Y": -6.7393193333333334 }, "geometry": { "type": "Point", "coordinates": [ 108.49251885, -6.739319333333333 ] } }
+]
+}
---
+++ data/PointofInterest_2.js
@@ -0,0 +1,19 @@
+var json_PointofInterest_2 = {
+"type": "FeatureCollection",
+"name": "PointofInterest_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Taman Wisata Goa Sunyaragi Cirebon", "desc": "Taman Wisata Goa Sunyaragi Cirebon, Sunyaragi, Kesambi, Cirebon City, West Java 45132, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 108.543495, -6.7364783 ] } },
+{ "type": "Feature", "properties": { "name": "Nasi Jamblang Ibu Nur", "desc": "Mrs. Nur's Jamblang Rice, Jl. Cangkring 2 No.34, Kejaksan, Cirebon City, West Java 45123, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 108.554994, -6.7144258 ] } },
+{ "type": "Feature", "properties": { "name": "Batu Lawang Cirebon", "desc": "Batu Lawang Cirebon, Cupang, Kec. Gempol, Cirebon, Jawa Barat 45161, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 108.3807464, -6.7277257 ] } },
+{ "type": "Feature", "properties": { "name": "Situ Cicerem", "desc": "Situ Cicerem, Jl. Kaduela, Kaduela, Pasawahan, Kabupaten Kuningan, Jawa Barat 45559, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 108.4230738, -6.7978055 ] } },
+{ "type": "Feature", "properties": { "name": "Bukit Gronggong", "desc": "Bukit Gronggong, Patapan, Beber, Cirebon, West Java 45172, Indonesia", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 108.521736, -6.7800977 ] } },
+{ "type": "Feature", "properties": { "name": "Rumah Sakit Ciremai Cirebon", "desc": null, "X": 108.549869, "Y": -6.738133, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 108.549869, -6.738133 ] } },
+{ "type": "Feature", "properties": { "name": "Masjid Al Jabar Plumbon", "desc": null, "X": 108.467773, "Y": -6.702311, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 108.467773, -6.702311 ] } },
+{ "type": "Feature", "properties": { "name": "Polres Kota Cirebon", "desc": null, "X": 108.474762, "Y": -6.758041, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 108.474762, -6.758041 ] } },
+{ "type": "Feature", "properties": { "name": "Polsek Gempol", "desc": null, "X": 108.423246, "Y": -6.706838, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 108.423246, -6.706838 ] } },
+{ "type": "Feature", "properties": { "name": "Masjid Mubarak", "desc": null, "X": 108.551183, "Y": -6.725766, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 108.551183, -6.725766 ] } },
+{ "type": "Feature", "properties": { "name": "SPBU Pertamina 34.451.57", "desc": null, "X": 108.549838, "Y": -6.727036, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 108.549838, -6.727036 ] } },
+{ "type": "Feature", "properties": { "name": "Rumah Sakit Sumber Hurip", "desc": null, "X": 108.46951, "Y": -6.757174, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 108.46951, -6.757174 ] } }
+]
+}
